5cb2b88e7d6b94a3f2080941d0bc25c32198eef0
Danch0 <iamguzmand@gmail.com>
2016-03-30 12:11:02 -0600

    Agregando simple auth como middleware

--- src/middleware.php
+++ src/middleware.php
@@ -6,18 +6,6 @@
 $app->add(function ($request, $response, $next) {
     // add media parser
     $request->registerMediaTypeParser(
-        "text/javascript",
-        function ($input) {
-            return json_decode($input, true);
-        }
-    );
-
-    return $next($request, $response);
-});
-
-$app->add(function ($request, $response, $next) {
-    // add media parser
-    $request->registerMediaTypeParser(
         "json",
         function ($input) {
             return json_decode($input, true);
@@ -40,4 +28,27 @@ $app->add(function ($request, $response, $next) {
     $newResp = $newResp->withAddedHeader("Access-Control-Allow-Headers", "origin, x-requested-with, content-type");
     $newResp = $newResp->withAddedHeader("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
     return $next($request, $newResp);
+});
+
+$app->add(function ($request, $response, $next) {
+    // echo $request->getHeader('PHP_AUTH_USER')[0].$request->getHeader('PHP_AUTH_PW')[0];
+    $myheaders = $request->getHeaders();
+    if(array_key_exists('PHP_AUTH_USER', $myheaders) && array_key_exists('PHP_AUTH_PW', $myheaders)){
+        if($request->getHeader('PHP_AUTH_USER')[0].$request->getHeader('PHP_AUTH_PW')[0] == "T53NTUCL0UDTRU350LUT10N5")
+            return $next($request, $response);
+        else {
+            $res = $response
+             ->withStatus(404)
+             ->withHeader('Content-Type', 'text/html')
+             ->write('Page not found');
+             return $res;
+        }
+    }
+    else {
+        $res = $response
+         ->withStatus(404)
+         ->withHeader('Content-Type', 'text/html')
+         ->write('Page not found');
+         return $res;
+    }
 });
