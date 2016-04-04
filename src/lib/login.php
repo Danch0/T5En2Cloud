2dfa047d9b21f34ae40bdc06d83823d2aba9d2d2
Danch0 <iamguzmand@gmail.com>
2016-04-04 12:42:40 -0500

    se agregaron los datos del usuario a la respuesta del login

--- src/lib/login.php
+++ src/lib/login.php
@@ -9,15 +9,27 @@ class Login extends MyController {
     * MyPDO(container: objeto contenedor de slim,"nombre de la tabla para PUT, DELETE, POST", "nombre de la tabla para GET", Obtenemos el objeto PDO del contenedor de slim para db)
     * este objeto lo asignamo a la variable $db de la clase Ficha
     */
-    parent::__construct($container,"usuario_ma","usuario_ma","id_usuario");
+    parent::__construct($container,"usuario_ma","vw_usuario_det","id_usuario");
   }
 	public function post($data = null) {
     $registro = $this->db->getRegistro(array('username_txt' => $data['username_txt']));
     // var_dump($registro);activo_bol
-    if(count($registro['result']) != 1)
-    	return array('estado'=>false,'mensaje'=>'ERROR: registro no se ha encontrado en la tabla.');
-    if (hash_equals($registro['result'][0]["password_txt"], crypt($data["password_txt"], ".TRU350LUT10N5}")))
-    	return array('estado'=>true, 'mensaje'=>'ok', 'result' => array('estado'=>true, 'mensaje'=>'ok','activo_bol'=>$registro['result'][0]["activo_bol"],'id_rol_rol_cat'=>$registro['result'][0]["id_rol_rol_cat"]));
+    if($registro['estado']) {
+      if(count($registro['result']) == 1){
+        if (hash_equals($registro['result'][0]["password_txt"], crypt($data["password_txt"], ".TRU350LUT10N5}"))) {
+          $usuario = $registro['result'][0];
+          unset($usuario['id_usuario']);
+          unset($usuario['id_doctor_doctor_ma']);
+          unset($usuario['password_txt']);
+          unset($usuario['fecha_alta_dt']);
+          unset($usuario['ultimo_login_dt']);
+          unset($usuario['deleted_bool']);
+          return array('estado'=>true, 'mensaje'=>'ok', 'result' => array('estado'=>true, 'mensaje'=>'ok','usuario'=>$usuario));
+        }
+      }
+      return array('estado'=>false,'mensaje'=>'Usuario o Contraseña incorrectos.');
+    }else
+      return array('estado'=>false,'mensaje'=>$registro['mensaje']);
 	}
 
 }
